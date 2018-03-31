30365d9ea80184da030e09fe0e028efc008495ab
Iqbal Rosiadi <ikibozu@MacBook-Pro.home>
2018-03-31 02:58:38 +0100

    edit post

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -146,7 +146,8 @@ exports.data_create_get = [
 // Display list of device
 exports.data_create_post = [
 	
-
+	//body('sensor_name', 'Sensor name must not be empty.').isLength({ min: 1 }).trim(),
+	//sanitizeBody('sensor_name').trim().escape(),
 	(req, res, next) => {
 
 		const errors = validationResult(req);
@@ -158,28 +159,101 @@ exports.data_create_post = [
 	          }
 	        );
 
+		console.log("name of the channel before " + data.value);
+
 		if (!errors.isEmpty()) {
 			res.send('Data is not complete');
 			return;
 
 		}else
 		{
+			
+					Sensor.findOne(
+					{"_id": req.params.id}, 
+					function(errs,sensor_detail)
+					{				
+						if(sensor_detail)
+						{
+							if(errs) { return next(errs);}
+								var start = new Date(new Date().getTime() - (sensor_detail.data_range_minute * 60 * 1000));
+								//console.log('DATA');
+								Data.find({ "date": { "$lte": start } }).select("date _id").exec(function(err,data_details){
+									if(err) { return next(err);}
+									console.log('paling lama :'+ data_details);
+										Data.deleteMany({"_id": { $in: data_details }}, function(errors){
+											console.log('deleting sensor paling lama :'+ data_details);
+											if(errors) { return next(errors);}
+											data.save(function (err){
+												if(err) { return next(err);}
+												//console.log('blaabla');
+												if(sensor_detail.calculation == 'summary'){
+													Data.aggregate([
+												        {$match: { 
+												        		"date": { "$gte": start },
+												        		"sensor": sensor_detail._id
+												    			}},
+												        {$group: {
+												        	_id: '$sensor',
+												        	average: {$sum: '$value'}
+												        }}
+												    ], function (err, result) {
+														if(err) { return next(err);}
+														Sensor.findByIdAndUpdate(sensor_detail._id, {data: result[0].average} , function (err, hasil) {
+												            if (errors) { return next(errors);}
+															res.send('Data is saved ini '+ result[0].average);
+												            
+												        });
+														
+													});
+												}
 
-			data.save(function (err){
-				if(err) { return next(err);}
-					Sensor.findOneAndUpdate(
-					{"_id": req.params.id}, {$push: {"data": data._id}} ,
-					function(errs,results)
-					{
-						if(errs) { return next(errz);}
-						results.save(function (err){
-								if(err) { return next(err);}
-									res.send('Data is saved blabla'+results.data_range_minute);
+												if(sensor_detail.calculation == 'average'){
+													Data.aggregate([
+												        {$match: { 
+												        		"date": { "$gte": start },
+												        		"sensor": sensor_detail._id
+												    			}},
+												        {$group: {
+												        	_id: '$sensor',
+												        	average: {$avg: '$value'}
+												        }}
+												    ], function (err, result) {
+														if(err) { return next(err);}
+														Sensor.findByIdAndUpdate(sensor_detail._id, {data: result[0].average} , function (err, hasil) {
+												            if (errors) { return next(errors);}
+															res.send('Data is saved ini '+ result[0].average);
+												            
+												        });
+														
+													});
+												}
 
-							});
+
+												if(sensor_detail.calculation == 'lastvalue'){
+													Data.findOne({'sensor': sensor_detail._id})
+													.sort({date: -1})
+													.exec(function(err, result) { 
+														if(err) { return next(err);}
+														Sensor.findByIdAndUpdate(sensor_detail._id, {data: result.value} , function (err, hasil) {
+												            if (err) { return next(err);}
+															res.send('Data is saved ini '+ result.value);
+												            
+												        });
+													});
+												}
+
+												});
+										});    
+								});
+						}else
+						{
+							res.send('Sensor is not found');
+							return;
+						}
+
 					});
 
-			});
+
 
 
 		}
