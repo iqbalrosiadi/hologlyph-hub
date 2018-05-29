2ac35103d5885f3e169fe6f3ba4f00cc96d619a7
Iqbal Rosiadi <ikibozu@MacBook-Pro.local>
2018-05-29 20:13:00 +0100

    edit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 // GET home page.
+/*
 router.get('/', function(req, res) {
   //res.redirect('/');
   res.render('sensor_list', { title: 'Hologyph Hub' });
-});
+}); */
+
+// GET request for list of all device items.
+router.get('/', device_controller.device_list);
 
 module.exports = router;
