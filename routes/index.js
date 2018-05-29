9424c705f99fa211062c3ad6dfdd488e68f5dfb2
Iqbal Rosiadi <ikibozu@MacBook-Pro.local>
2018-05-29 20:09:09 +0100

    edit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // GET home page.
 router.get('/', function(req, res) {
   //res.redirect('/');
-  res.render('index', { title: 'Hologyph Hub' });
+  res.render('sensor_list', { title: 'Hologyph Hub' });
 });
 
 module.exports = router;
